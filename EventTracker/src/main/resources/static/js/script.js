b431743b20e69171442e1d56a61885fb3d2d9341
KerwinJT <kerwinjt@gmail.com>
2020-04-17 15:53:17 -0600

    Stub HTML/JS for front end CRUD

---
+++ EventTracker/src/main/resources/static/js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', evt =>{
+	console.log('Window loaded');
+	init();
+});
+
+function init(){
+	console.log('In init function');
+	console.log('Didnt return to eclipse');
+	
+	
+	
+}
